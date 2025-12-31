cd95588ea9f97b74c607b06fcb1ca1b6e6954a92
Nagashreesbhovi <nsbhovi2005@gmail.com>
2026-01-01 00:51:32 +0530

    Car Selling web

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function showMessage() {
+    alert("Thank you for choosing CarZone!");
+function showDetails(carName) {
+    alert(
+        carName + " is available!\n\nPlease contact us for more details."
+    );
+}
+}
+ 
